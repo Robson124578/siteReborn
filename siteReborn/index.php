35828d86cd108c9351dec249480994816ad615fd
Robson124578 <robim.net@gmail.com>
2019-03-11 01:19:40 -0300

    11-03-2019 / desenvolvendo a estrutura da página (Maria Paula, arte reborn)

---
+++ siteReborn/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Maria Flor.</title>
+	  <meta charset="UTF-8">
+  	  <meta name="description" content="Site de Bebês Reborn">
+  	  <meta name="keywords" content="Reborn, bebe, boneca, menina">
+  	  <meta name="author" content="Robson Alves">
+  	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	  <link rel="stylesheet" type="text/css" href="css/stylo.css">
+</head>
+<body>
+	<div class="container-geral row col-10">
+		<nav class="container-menu row">
+			
+		</nav>
+		<footer class="rodape row col-12">
+			
+		</footer>
+	</div>
+</body>
+</html>
